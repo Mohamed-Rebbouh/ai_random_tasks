f771c3486cbfb57f7873c7bd3a769e4dfb9278d8
Mohamed-Rebbouh <121132111+Mohamed-Rebbouh@users.noreply.github.com>
2024-05-11 12:21:32 +0100

    Create logistiqueReggression.py

---
+++ logistiqueReggression.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, learning_rate=0.01, num_iterations=1000):
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X, y):
+        self.theta = np.zeros(X.shape[1])
+
+        for _ in range(self.num_iterations):
+            z = np.dot(X, self.theta)
+            h = self.sigmoid(z)
+            gradient = np.dot(X.T, (h - y)) / y.size
+            self.theta -= self.learning_rate * gradient
+
+    def predict(self, X):
+        z = np.dot(X, self.theta)
+        return np.round(self.sigmoid(z))
